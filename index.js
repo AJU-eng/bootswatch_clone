0a8024be2791c486df81cedd973785b632e6fc8b
Ajay <aju3314@gmail.com>
2023-07-09 13:20:52 +0530

    js changed

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-window.addEventListener('scroll',()=>{
-    var nav = document.getElementById("nav");
-    nav.style.backgroundColor="rgb(93,71,224)";
-})
+const nav = document.getElementById('nav');
+window.onscroll= function () {
+    if (document.body.scrollTop >= 200) {
+        nav.style.backgroundColor="rgb(93,71,224)"
+        console.log("changed");
+    }
+    else
+    {
+        nav.style.backgroundImage=" linear-gradient(145deg, #375ee3 0%, #6543e0 80%)"
+        console.log("back changed");
+    }
+}
