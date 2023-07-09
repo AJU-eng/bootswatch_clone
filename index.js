fa3b196cdabc245df3430050cbfe32bc10cd0197
Ajay <aju3314@gmail.com>
2023-07-09 18:07:01 +0530

    fouth committ

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const nav = document.getElementById('nav');
 window.onscroll= function () {
-    if (document.body.scrollTop >= 200) {
-        nav.style.backgroundColor="rgb(93,71,224)"
+    if (document.documentElement.scrollTop >= 200) {
+        nav.style.backgroundColor="rgb(93,71,224)";
         console.log("changed");
     }
     else
     {
-        nav.style.backgroundImage=" linear-gradient(145deg, #375ee3 0%, #6543e0 80%)"
+        nav.style.backgroundColor="rgba(0,0,0,0)";
         console.log("back changed");
     }
 }
